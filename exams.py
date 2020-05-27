392bac3c4962e10a0c9387d3999fa5d200c25a4f
kosmokato <58893728+kosmokato@users.noreply.github.com>
2020-05-27 08:17:12 +0200

    Roll until die!
    Now the game will test you until you fail, adding points to your score.
    
    The questions and the answers are truthly random, good luck!

--- exams.py
+++ exams.py
@@ -2,13 +2,14 @@ import random
 import os, sys
 
 def quiz(dict_array):
+	right_ones = 0
 	quiz_answer = 5
 	quiz_options = []
-	i = random.randint(0,len(dict_array)-1)
+	print("\nQUIZ\n-----\n")
 	while(quiz_answer not in [0,1,2,3]):
+		i = random.randint(0,len(dict_array)-1)
 		try:
-			print("\nQUIZ\n-----\n")
-			print("What does it means \"" + dict_array[i]["chinese"] + "\" (" + dict_array[i]["pinyin"] + ")?")
+			print("\nWhat does it means \"" + dict_array[i]["chinese"] + "\" (" + dict_array[i]["pinyin"] + ")?")
 			
 			quiz_options = []
 			
@@ -17,7 +18,7 @@ def quiz(dict_array):
 			while len(answers) < 4:
 				answers.add(random.randint(0, len(dict_array)-1))
 
-			for x in answers:  # add the answers to the quiz
+			for x in answers:
 				quiz_options.append(dict_array[x]["translation"])  
 
 			random.shuffle(quiz_options)
@@ -26,16 +27,23 @@ def quiz(dict_array):
 				print("\t[" + str(x) + "] " + quiz_options[x])
 			quiz_answer = int(input("Choose an option:"))
 
-			if quiz_options[quiz_answer] == dict_array[i]["translation"]: print("[+] 六六六!")
+			if quiz_options[quiz_answer] == dict_array[i]["translation"]: 
+				print("[+] 六六六!")
+				right_ones += 1
+				print("    You won 5 points! [" + str(right_ones * 5) + "]\n")
 			else: 
 				print("[-] Wrong answer! :_(")
-				print("    The right answer was \"" + dict_array[i]["translation"] + "\"\n\n")
+				print("    The right answer was \"" + dict_array[i]["translation"] + "\"\n")
+				print("You made " + str(right_ones * 5) + " points!")
+				print("祝您下次运气更好 ^_^")
+				exit()  # <---- THIS MUST BE CHANGED
 
-			if quiz_answer in [0,1,2,3]: break  # valid option, end for now  <---- THIS MUST BE CHANGED
+			if quiz_answer in [0,1,2,3]: quiz_answer = 5  # <---- THIS MUST BE CHANGED
 			else: 
-				print("[i] Invalid answer! try again ;)\n\n")	
-		except KeyboardInterrupt:
-			exit()  # if Ctrl+C, abort
+				print("[i] Invalid answer! try again ;)\n\n")
+				
+		except KeyboardInterrupt: exit()  # if Ctrl+C, abort
+		except ValueError: pass
 		except Exception as e:
 			exc_type, exc_obj, exc_tb = sys.exc_info()
 			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
